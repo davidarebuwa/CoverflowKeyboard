bc98c6053ba3a323411344c9c8cbe8fea28fd71d
Dominic Drees <dominic.drees@atino.de>
2020-07-06 23:06:32 +0200

    further implementation

---
+++ Example/CoverflowKeyboard/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CoverflowKeyboard_Example
+//
+//  Created by Dominic Drees on 06.07.20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        super.tableView(tableView, didSelectRowAt: indexPath)
+        
+        
+    }
+}
