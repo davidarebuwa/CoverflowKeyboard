0f8fc96757fdc806d8efb26fa815a52d65216244
Dominic Drees <dominic.drees@atino.de>
2020-07-07 13:22:38 +0200

    removed gitignore

---
+++ Example/Pods/Target Support Files/Pods-CoverflowKeyboard_Example/Pods-CoverflowKeyboard_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CoverflowKeyboard_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CoverflowKeyboard_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-CoverflowKeyboard_Tests/Pods-CoverflowKeyboard_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CoverflowKeyboard_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CoverflowKeyboard_Tests
+@end
