3d221d594c2ab37ad79b2748ea7ff41688c85c05
David Arebuwa <davidarebuwa.gmail.com>
2020-07-08 13:21:10 +0100

    minor progress

--- Example/CoverflowKeyboard/ViewController.swift
+++ Example/CoverflowKeyboard/ViewController.swift
@@ -7,16 +7,45 @@
 //
 
 import UIKit
+import CoverflowKeyboard
 
 class ViewController: UITableViewController {
     
+    @IBOutlet weak var sampleButton: UIButton!
+    @IBOutlet weak var sampleBarButton: UIBarButtonItem!
+    @IBOutlet weak var sampleTextField: UITextField!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupSampleButton()
     }
+    
+    func setupSampleButton(){
+        let dummyTF = UITextField(frame: .zero)
+        dummyTF.placeholder = "Emoji Coverflow"
+        sampleButton.addSubview(dummyTF)
+       //Coverflow stuff
+        let coverFlow = CoverflowKeyboard()
+       
+        
+        dummyTF.becomeFirstResponder()
+    }
+    
+    func emojiCoverFlow(){}
+    func imageCoverFlow(){}
+    func customCoverFlow(){}
+    
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         super.tableView(tableView, didSelectRowAt: indexPath)
+        switch(indexPath.section, indexPath.row) {
+        case (0, 0):
+            print("selected")
         
-        
+        default:
+            print("selected")
+        }
     }
 }
--- Sources/CoverflowKeyboard/Classes/CoverflowKeyboard.swift
+++ Sources/CoverflowKeyboard/Classes/CoverflowKeyboard.swift
@@ -3,15 +3,19 @@
 //  CoverflowKeyboard
 //
 //  Created by Dominic Drees on 06.07.20.
-//
+//  this will be the wrapper thats accessible from outside
+// needs to be extra configurable ( to maximise user happines )
+
+// make all classes private that should not be accessible from outside
+// make all classes final that should not be subclassed from outside aswell :P
 
 import UIKit
 
 public final class CoverflowKeyboard {
-    
-    // this will be the wrapper thats accessible from outside
-    // needs to be extra configurable ( to maximise user happines )
- 
-    // make all classes private that should not be accessible from outside
-    // make all classes final that should not be subclassed from outside aswell :P
+    var inputView : CoverFlowInputView?
+    var data: [CellData] = []
+  
+    public init(){
+        inputView = CoverFlowInputView()
+    }
 }
